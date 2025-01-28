4c5bc57bf948443957539aeaa5d818ae48a3433e
Ricardo <156342546+ricandresfriki97@users.noreply.github.com>
2025-01-28 20:33:24 -0300

    Add files via upload

---
+++ ciclowhile.java
@@ -0,0 +1,9 @@
+public class ciclowhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <=5) {
+            System.out.println("Iteracion" + i);
+            i++;
+        }
+    }
+}
