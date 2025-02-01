eaf7fb13c34c5ab8588c0af445e4f811efebf03d
Ricardo <156342546+ricandresfriki97@users.noreply.github.com>
2025-02-01 11:02:47 -0300

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -1,51 +1,55 @@
 import java.util.Scanner;
-
+////////////////////////////////////////////////
 public class calculadora {
-    public static void main (String[] args) {
+    public static void main (String[] args){
+////////////////////////////////////////////////
         Scanner sc = new Scanner(System.in);
-
-
+////////////////////////////////////////////////
         System.out.print("ingrese el primer numero");
-        double num1 = sc.nextDouble();
-
-        System.out.print("ingrese el primer numero");
-        double num2 = sc.nextDouble();
-
-
-        System.out.print("elija la operacion (+, -. *, /): ");
-        char operacion = sc.next().charAt(0);
-
-        double resultado;
-
-
-
-
-        switch (operacion) {
+        double numero1 = sc.nextDouble();
+        System.out.print("ingrese el segundo numero");
+        double numero2 = sc.nextDouble();
+        System.out.print("ingrese la operacion a realizar + - * /");
+        char operation = sc.next().charAt(0);
+        double result;
+//////////////////////////////////////////////////
+        switch (operation){
             case '+':
-            resultado = num1 + num2;
-            System.out.println("resultado" + resultado);
+            result = numero1 + numero2;
+            System.out.println("resultado"+resultado);
             break;
             case '-':
-            resultado = num1 - num2;
-            System.out.println("resultado" + resultado);
+            result - numero1 - numero2:
+            System.out.println("resultado"+resultado);
             break;
             case '*':
-            resultado = num1 * num2;
-            System.out.println("resultado" + resultado);
+            result = numero1 * numero2:
+            System.out.println("resultado"+resultado):
             break;
             case '/':
-                if (num2 != 0) {
-                    resultado = num1 / num2;
-                    System.out.println("resultado" + resultado);
-                } else {
-                   System.out.println("error no se puede dividir por cero");
+            if (numero2 != 0){
+                result = numero1 / numero2:
+                System.out.println("resultado"+resultado);
+            else {
+                System.out.println("no se puede dividir por cero");
 
-                }
-                break;
+            }
+            break;
+///////////////////////////////////////////////////            
             default:
             System.out.println("operacion no valida");
-            break;    
+            break;
             }
-                sc.close();
+//////////////////////////////////////////////////
+            sc.close();
+        }          
+
+
+
+
+
+
+
+
+
     }
-}
