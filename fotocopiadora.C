2ddd483cb40a2ad832c1a268a56bfd46d6124d58
Ricardo <156342546+ricandresfriki97@users.noreply.github.com>
2025-02-01 21:10:31 -0300

    Update fotocopiadora.C

--- fotocopiadora.C
+++ fotocopiadora.C
@@ -1,36 +1,27 @@
 #include <stdio.h>
-
+//////////////////////////
 int main() {
     int hojas;
     float total;
-
-    // Limpiar pantalla (solo en Windows)
-    system("cls");
-
-    // Se ingresa la cantidad de hojas
-    printf("Ingresa la cantidad de hojas: ");
-    scanf("%d", &hojas);
-
-    // Cada hoja vale $20
-    total = 20 * hojas;
-
-    // Si hay más de 30 hojas, se aplica un 10% de descuento
-    if (hojas > 30) {
-        total = total * 0.90;
-    }
-
-    // Pausa antes de limpiar pantalla (solo en Windows)
-    system("pause");
-
-    // Limpiar pantalla (solo en Windows)
-    system("cls");
-
-    // Imprimir boleta
-    printf("\n    ----  BOLETA -----\n");
-    printf("    Cant. hojas:  %d\n", hojas);
-    printf("    Valor por hoja $20\n");
-    printf("    TOTAL $%.2f\n", total);
-
-    return 0;
+//////////////////////////
+system('clear');
+//////////////////////////
+printf("ingresa la cantidad de hojas");
+scanf("%d", &hojas);
+//////////////////////////
+total = 20* hojas;
+/////////////////////////
+if (hojas>30) {
+    total = total *0.90;
 }
+//////////////////////////
+system('clear');
+//////////////////////////
+printf(********boleta de pago********);
+printf("cantidad de hojas: %d\n", hojas)
+printf("total a pagar: $%.2f\n", total);
+return 0;
+///////////////////////////
+}
+///////////////////////////
 
