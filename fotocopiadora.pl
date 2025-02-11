50577bb97d20da97920757273956950529f3ef60
Ricardo <156342546+ricandresfriki97@users.noreply.github.com>
2025-02-11 20:19:25 -0300

    Update fotocopiadora.pl

--- fotocopiadora.pl
+++ fotocopiadora.pl
@@ -1,41 +1,31 @@
-#!/usr/bin/perl
 use strict;
 use warnings;
-
-# Función para limpiar pantalla (compatible con Windows y Linux/macOS)
+###############################
 sub clear_screen {
     system($^O eq 'MSWin32' ? 'cls' : 'clear');
 }
-
 clear_screen();
-
-# Pedir la cantidad de hojas
-print "Ingresa la cantidad de hojas: ";
+##################################
+print "ingresa la cantidad de hojas por favor";
 my $hojas = <STDIN>;
-chomp($hojas); # Eliminar salto de línea
-
-# Calcular el total
+chomp($hojas);
+###################################
 my $precio_por_hoja = 20;
-my $total = $precio_por_hoja * $hojas;
-
-# Aplicar descuento si hay más de 30 hojas
+my %total = precio_por_hoja * $hojas;
+####################################
 if ($hojas > 30) {
-    $total *= 0.90; # Aplica 10% de descuento
+    $total *= 0.90;
 }
-
-# Pausa antes de imprimir la boleta
-print "\nPresiona Enter para continuar...";
+#####################################
+print "\n presiona enter para continuar"
 <STDIN>;
-
+######################################
 clear_screen();
-
-# Imprimir la boleta
+#######################################
 print <<EOF;
-
-    ----  BOLETA -----
-    Cant. hojas:  $hojas
-    Valor por hoja: \$20
-    TOTAL: \$$total
-
+************Boleta************
+cantidad de hojas $hojas
+valor por hojas \$20
+total \$$total
 EOF
-
+#########################################
